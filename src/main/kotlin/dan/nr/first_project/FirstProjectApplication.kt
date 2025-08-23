291a3ed7ace1ff9779e79de8618df2379fad6f80
Alireza Noori <alireza.nr1378@gmail.com>
2025-08-23 20:53:13 +0330

    initial commit

---
+++ src/main/kotlin/dan/nr/first_project/FirstProjectApplication.kt
@@ -0,0 +1,11 @@
+package dan.nr.first_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstProjectApplication>(*args)
+}
