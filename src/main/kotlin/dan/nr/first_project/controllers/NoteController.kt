16a8ced5ee366594fa5661b2c5a71e26851ddddb
Alireza Noori <alireza.nr1378@gmail.com>
2025-09-04 13:50:05 -0700

    implement delete note functionality in NoteController

--- src/main/kotlin/dan/nr/first_project/controllers/NoteController.kt
+++ src/main/kotlin/dan/nr/first_project/controllers/NoteController.kt
@@ -4,7 +4,9 @@ import dan.nr.first_project.controllers.NoteController.NoteResponse
 import dan.nr.first_project.database.model.Note
 import dan.nr.first_project.database.repository.NoteRepository
 import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -59,6 +61,13 @@ class NoteController(
         return repository.findByOwnerId(ObjectId(ownerId)).map {
             it.mapToResponse()
         }
+    }
+
+    @DeleteMapping(path = ["/{id}"])
+    fun deleteById(
+        @PathVariable id: String
+    ) {
+        repository.deleteById(ObjectId(id))
     }
 }
 
