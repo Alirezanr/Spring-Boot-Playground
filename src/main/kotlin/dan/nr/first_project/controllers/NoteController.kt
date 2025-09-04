8b4095bd7cebbcc4f34182e14ef0fc0bb6fe4313
Alireza Noori <alireza.nr1378@gmail.com>
2025-09-04 04:18:44 -0700

    create NoteController

---
+++ src/main/kotlin/dan/nr/first_project/controllers/NoteController.kt
@@ -0,0 +1,71 @@
+package dan.nr.first_project.controllers
+
+import dan.nr.first_project.controllers.NoteController.NoteResponse
+import dan.nr.first_project.database.model.Note
+import dan.nr.first_project.database.repository.NoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+
+@RestController
+@RequestMapping("/notes")
+class NoteController(
+    private val repository: NoteRepository
+) {
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val ownerId: String,
+    )
+
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant
+    )
+
+    @PostMapping
+    fun save(body: NoteRequest): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId(body.ownerId)
+            )
+        )
+
+        return note.mapToResponse()
+    }
+
+    @GetMapping
+    fun findByOwnerId(
+        @RequestParam(required = true) ownerId: String,
+    ): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+            it.mapToResponse()
+        }
+    }
+}
+
+
+private fun Note.mapToResponse(): NoteResponse {
+    return NoteResponse(
+        id = this.id.toHexString(),
+        title = this.title,
+        content = this.content,
+        color = this.color,
+        createdAt = this.createdAt,
+    )
+}
