6879f519a5fd4290c5c3f30e1d4e0f9ec04dcfea
Alireza Noori <alireza.nr1378@gmail.com>
2025-09-21 11:04:27 -0700

    Inherit UserRepository from MongoRepository

--- src/main/kotlin/dan/nr/first_project/database/repository/UserRepository.kt
+++ src/main/kotlin/dan/nr/first_project/database/repository/UserRepository.kt
@@ -1,7 +1,9 @@
 package dan.nr.first_project.database.repository
 
 import dan.nr.first_project.database.model.User
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
 
-interface UserRepository {
+interface UserRepository : MongoRepository<User, ObjectId> {
     fun findByEmail(email: String): User?
 }
